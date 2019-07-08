873784c8731f372af5737a99dcc05bda2b8cad34
themother <jacktowleretc@hotmail.com>
2019-07-08 21:42:29 +0100

    base window

---
+++ src/main/java/com/jtowler/synth/SynthesizerRemastered.java
@@ -0,0 +1,18 @@
+package com.jtowler.synth;
+
+import javax.swing.*;
+
+public class SynthesizerRemastered {
+
+    private JFrame frame = new JFrame("Synthesizer Remastered");
+
+    SynthesizerRemastered() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(613, 357);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+}
